8b1629017a1d940ab13333ba8d812279987729e1
Alen Thomas <alenthomas9@gmail.com>
2017-08-30 11:02:45 +0530

    adds react element for imgs

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ var rootElement =
     React.createElement('ul', {},
       React.createElement('li', {},
 	React.createElement('h2', {}, 'Rick Sanchez'),
+        React.createElement('img', {src: 'https://vignette3.wikia.nocookie.net/rickandmorty/images/a/a6/Rick_Sanchez.png/revision/latest/scale-to-width-down/376?cb=20160923150728'}),
+        React.createElement('br'),
     	React.createElement('a', {href: 'http://rickandmorty.wikia.com/wiki/Rick_Sanchez'}, 'Rick C-137')
       ),
       React.createElement('li', {},
 	React.createElement('h2', {}, 'Morty Smith'),
+        React.createElement('img', {src: 'https://vignette4.wikia.nocookie.net/rickandmorty/images/4/41/Morty_Smith.jpg/revision/latest/scale-to-width-down/301?cb=20170217193441'}),
+        React.createElement('br'),
 	React.createElement('a', {href: 'http://rickandmorty.wikia.com/wiki/Morty_Smith'}, 'Morty C-137')
       )
     )
